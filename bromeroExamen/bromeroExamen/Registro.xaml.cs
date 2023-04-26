87bb345649b18bb45a73d72b6dfa7b7b06ddb145
Bryan Romero <Bryan Romero@DESKTOP-ATSQLL6>
2023-04-25 21:11:20 -0500

    Agregar archivos de proyecto.

---
+++ bromeroExamen/bromeroExamen/Registro.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bromeroExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            txtUsuario.Text = usuario;
+
+        }
+
+        private void btnCalcularPago_Clicked(object sender, EventArgs e)
+        {
+            double Minicial = double.Parse(txtMinicial.Text);
+            double Pagomensual = 0.00;
+            double recargo = 0.05;
+            if (Minicial < 3000 && Minicial >= 0)
+            {
+                Pagomensual = ((3000 - Minicial) / 3) + (recargo * 3000);
+                txtPago.Text = Pagomensual.ToString();
+            }
+            else {
+                DisplayAlert("Alerta", "Monto Minimo debe ser menor a 3000 y mayor a 0", "Cerrar");
+                txtMinicial.Text = "";
+
+            }
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Alerta", "Elemento guardado con exito", "Cerrar");
+
+            Navigation.PushAsync(new Resumen(txtUsuario.Text, txtPago.Text, txtNombre.Text, txtMinicial.Text));
+
+        }
+    }
+}
---
+++ bromeroExamen/bromeroExamen/Resumen.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bromeroExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+        public Resumen(string usuario, string Pagomensual, string nombre, string Minical)
+        {
+            InitializeComponent();
+            txtUsuario.Text = usuario;
+            txtNombre.Text = nombre;
+            double datoUno = Convert.ToDouble(Pagomensual);
+            double datoDos = Convert.ToDouble(Minical);
+
+            double total = ((datoDos) + (datoUno * 3));
+            txtPagoMensual.Text = total.ToString();
+
+        }
+    }
+}
